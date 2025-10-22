ec1a6569c6fb0330e5fc05cf1f636fe040296d88
jorge gonzalez <gonzalezjor13@gmail.com>
2025-10-21 22:11:11 -0500

    api user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: serialized(@user, UserSerializer), status: 200
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: serialized(@user, UserSerializer), status: :created
+    else
+      render json: { message: "Error al crear el usuario" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroyrender json: {message: "Usuario eliminado correctamente"}, status: 200
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Invalid email" }
+  validates :password, presence: true, length: { minimum: 6 }
+end
